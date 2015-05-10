3f63fe3aae339fc3b09fe3d3ae380b345c9ba6fc
Alejandro U. Alvarez <aluralma@gmail.com>
2015-05-10 18:56:03 +0100

    CORS support

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ passport.deserializeUser(function(id, done) {
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT || 'http://localhost:8000');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
