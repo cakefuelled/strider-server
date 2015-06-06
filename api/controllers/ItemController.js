314f8cb29fc53efc080b4080c4fe7e04597055cd
Bharat Reddy <me@bharatreddy.com>
2015-06-06 16:09:32 +0100

    Parking sails

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -7,5 +7,28 @@
 
 module.exports = {
 	
+  getItemsByOrganisation: function(req, res) {
+
+    console.log(req.orgId);
+
+    Organisation
+      .findOne({ organisation: req.orgId })
+      .populate('items')
+      .exec(function(err, org) {
+        console.log("Yay1");
+        console.log(org);
+        console.log(org.items);
+      });
+
+    res.send(org.items);
+
+    /*Item
+      .findByOrganisation(req.orgId)
+      .exec(function(err,org) {
+        console.log("YAY");
+        console.log(org);
+      });*/
+  }
+
 };
 
