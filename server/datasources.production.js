1500bfe25e639b14e876802c3ef2a85933ace5e2
Alejandro U. Alvarez <aluralma@gmail.com>
2015-06-27 13:59:12 +0100

    local admin user

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,6 +2,7 @@ module.exports = {
   Mongodb: {
     name: 'Mongodb',
     connector: 'mongodb',
+    database: process.env.MONGO_DB,
     user: process.env.MONGO_USER,
     password: process.env.MONGO_USER
   }
